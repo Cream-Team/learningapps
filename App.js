55efd712627282a996cd9dfb895ea28f1875de74
Macgia99 <macgia1999@gmail.com>
2021-04-18 12:08:50 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
-import React, { useState } from "react";
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import { createDrawerNavigator } from "react-navigation-drawer";
-import AppLoading from "expo-app-loading";
+import React, {useState} from 'react';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+import AppLoading from 'expo-app-loading';
 
-import * as Font from "expo-font";
-import Mainactivity from "./src/screens/Mainactivity";
-import SidemenuNotloggedin from "./src/screens/SidemenuNotloggedin";
-import SidemenuLoggedin from "./src/screens/SidemenuLoggedin";
-import Ontap from "./src/screens/Ontap";
-import Baihoc from "./src/screens/Baihoc";
-import Thongke from "./src/screens/Thongke";
-import Setting from "./src/screens/Setting";
-import Chonbaihoc from "./src/screens/Chonbaihoc";
-import QnA from "./src/screens/QnA";
-import Info from "./src/screens/Info";
-import Videos from "./src/screens/Videos";
-import ExtendLink from "./src/screens/ExtendLink";
+import * as Font from 'expo-font';
+import Mainactivity from './src/screens/Mainactivity';
+import SidemenuNotloggedin from './src/screens/SidemenuNotloggedin';
+import SidemenuLoggedin from './src/screens/SidemenuLoggedin';
+import Ontap from './src/screens/Ontap';
+import Baihoc from './src/screens/Baihoc';
+import Thongke from './src/screens/Thongke';
+import Setting from './src/screens/Setting';
+import Chonbaihoc from './src/screens/Chonbaihoc';
+import QnA from './src/screens/QnA';
+import Info from './src/screens/Info';
+import Videos from './src/screens/Videos';
+import ExtendLink from './src/screens/ExtendLink';
 
 const DrawerNavigation = createDrawerNavigator({
   Mainactivity: Mainactivity,
@@ -30,13 +30,13 @@ const DrawerNavigation = createDrawerNavigator({
   QnA: QnA,
   Info: Info,
   Videos: Videos,
-  ExtendLink: ExtendLink
+  ExtendLink: ExtendLink,
 });
 
 const StackNavigation = createStackNavigator(
   {
     DrawerNavigation: {
-      screen: DrawerNavigation
+      screen: DrawerNavigation,
     },
     Mainactivity: Mainactivity,
     SidemenuNotloggedin: SidemenuNotloggedin,
@@ -49,11 +49,11 @@ const StackNavigation = createStackNavigator(
     QnA: QnA,
     Info: Info,
     Videos: Videos,
-    ExtendLink: ExtendLink
+    ExtendLink: ExtendLink,
   },
   {
-    headerMode: "none"
-  }
+    headerMode: 'none',
+  },
 );
 
 const AppContainer = createAppContainer(StackNavigation);
